5b1fd4602c9d3af586affae5b9c618586eb12fa6
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-10-12 21:04:40 +0300

    Initial Commit

---
+++ RenderingElements/RenderingElementsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RenderingElementsApp.swift
+//  RenderingElements
+//
+//  Created by Ramill Ibragimov on 12.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct RenderingElementsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
